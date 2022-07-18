4f86e03848704ae6588e1d8d455f894298b3ccf4
farisqadr <faris.code@gmail.com>
2022-07-18 20:40:43 +0700

    title

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -5,19 +5,46 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
+        body: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 100, vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            'Halo Semuanya!',
-            style: GoogleFonts.poppins(fontSize: 30),
+          Image.asset(
+            'logo.png',
+            width: 72,
+            height: 40,
           ),
-          Text(
-            'Halo Teman - teman!',
-            style: TextStyle(fontSize: 30),
+          Row(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Guide',
+                    style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: Color(0xff1D1E3C),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Container(
+                    width: 66,
+                    height: 2,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xffFE998D)),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Image.asset(
+            'btn_myaccount.png',
+            width: 163,
+            height: 53,
           )
         ],
-      )),
-    );
+      ),
+    ));
   }
 }
